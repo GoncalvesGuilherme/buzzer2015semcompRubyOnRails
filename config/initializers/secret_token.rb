166dff9105f075222a23e13ec926767e006436ca
GoncalvesGuilherme <guilherme3.goncalves@usp.br>
2015-08-18 16:58:33 -0300

    buzzer on rails added to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Buzzer::Application.config.secret_token = '87543e638b283ff27348882f8d02d78ad551cbae165faf0b0579d2f8e608dbd1b0206ab701d4be94331b3e6a030d3d2f8a0276d477fff462a59408dc488779f7'
